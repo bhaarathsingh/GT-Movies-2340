35bec812b384df3a43aaa5ec228e317604c69241
bhaarathsingh <bhaaraths20@gmail.com>
2025-02-01 16:48:53 -0500

    Worked on Chapter 3

--- moviesstore/urls.py
+++ moviesstore/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for CS2340_Project1 project.
+URL configuration for moviesstore project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -14,12 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
-
-from MoviesProject import admin
+from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('home.urls')),
 ]
