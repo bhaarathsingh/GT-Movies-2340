636f8b64002ff91d9ee54ba202908db66477256c
rithikdhivakar <rithikdhiva@gmail.com>
2025-02-04 17:20:36 -0500

    added

---
+++ movies/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Movie
+class MovieAdmin(admin.ModelAdmin):
+    ordering = ['name']
+    search_fields = ['name']
+admin.site.register(Movie, MovieAdmin)
