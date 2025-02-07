5b44c37471435585a356f8c1471fe66b0bc17b68
bhaarathsingh <bhaaraths20@gmail.com>
2025-02-07 10:40:55 -0500

    trial

--- moviesstore/urls.py
+++ moviesstore/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', include('home.urls')),
     path('movies/', include('movies.urls')),
     path('accounts/', include('accounts.urls')),
+    path('cart/', include('cart.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
